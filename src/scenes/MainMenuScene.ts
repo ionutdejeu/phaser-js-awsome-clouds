b74177d68cd13fb0758680f774c81de7870efa5e
ionutdejeu <ionut.dejeu@gmail.com>
2021-12-12 00:16:20 +0200

    Simple ECS

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -1,11 +1,59 @@
 
+import {
+	createWorld,
+	Types,
+	defineComponent,
+	defineQuery,
+	addEntity,
+	addComponent,
+	defineSystem,
+	IWorld,
+	System
+  } from 'bitecs'
+import { IWorldDefinition } from 'matter'
+
+const Vector3 = { x: Types.f32, y: Types.f32, z: Types.f32 }
+const Position = defineComponent(Vector3)
+const posQuery = defineQuery([Position])
+
+const world = createWorld()
+
+const createDummySystem = (scene:Phaser.Scene)=>{
+	return defineSystem((world,...args:any[])=>{
+		const positionEntities = posQuery(world);
+		for(let i = 0;i<positionEntities.length;i++){
+			console.log('Id:',positionEntities[i]);
+		}
+		return world;
+	});
+}
+const timeSystem = world => {
+	const { time } = world
+	const now = performance.now()
+	const delta = now - time.then
+	time.delta = delta
+	time.elapsed += delta
+	time.then = now
+	return world
+  }
 
 export default class MainMenuScene extends Phaser.Scene {
 	
 	backgroundGradient:Phaser.GameObjects.Graphics;
+	private world:IWorld;
+	private dummySystem:System;
     constructor() {
 		super("MainMenuScene"); 
+		this.world = world;
 		
+		const e = addEntity(world);
+		addComponent(world,Position,e);
+		Position.x[e]=10;
+		Position.y[e]=100;
+		Position.z[e]=100;
+		
+		this.dummySystem = createDummySystem(this);
+
 	}
 
     init(): void {
@@ -19,9 +67,7 @@ export default class MainMenuScene extends Phaser.Scene {
 	}
 
 	update(){
-		
+		this.dummySystem(this.world,"dwadaw",1,234,2,2,5);
 	}
-
-	 
 
 }
